aab4c262f0b6118f48650507be793abc86f3c790
Kevin Bi <keveloper@gmail.com>
2019-07-24 19:52:27 -0400

    added backup code

---
+++ backups/surveys/backup_surveys.py
@@ -0,0 +1,50 @@
+import firebase_admin
+import os
+from firebase_admin import credentials
+from firebase_admin import firestore
+from datetime import datetime
+import json
+
+def main():
+    init_survey_firebase()
+    db = get_survey_firestore_client()
+    backup_collection(db, "emails")
+    backup_collection(db, "responses")
+
+def backup_collection(db, collection_name):
+    collection_ref = db.collection(collection_name)
+    docs = collection_ref.stream()
+    dicts = {}
+    for doc in docs:
+        dicts[doc.id] = doc.to_dict()
+    output_json = json.dumps(dicts, default=datetime_converter)
+    output_filename = get_backup_filename(collection_name)
+    with open(output_filename, "w+") as f:
+        f.write(output_json)
+
+def init_survey_firebase():
+    # we're on the server, use the project ID
+    if not os.path.exists("survey_creds.json"):
+        cred = credentials.ApplicationDefault()
+        firebase_admin.initialize_app(cred, {
+            'projectId': "hodp-surveys",
+        })
+    # locally testing, we have some credential file
+    else:
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'survey_creds.json'
+        cred = credentials.ApplicationDefault()
+        firebase_admin.initialize_app(cred)
+
+def get_survey_firestore_client():
+    return firestore.client()
+
+def get_backup_filename(collection_name):
+    today = datetime.today()
+    return "{}_{}{}{}_backup.json".format(collection_name, str(today.year).zfill(4), str(today.month).zfill(2), str(today.day).zfill(2))
+
+def datetime_converter(o):
+    if isinstance(o, datetime):
+        return o.__str__()
+
+if __name__ == "__main__":
+    main()
