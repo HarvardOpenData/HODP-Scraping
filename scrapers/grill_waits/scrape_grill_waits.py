b1cc1986064162b530e641512c6098085c4d13b8
Petey <pdotjpr@gmail.com>
2019-10-12 18:13:13 -0400

    import sys

--- scrapers/grill_waits/scrape_grill_waits.py
+++ scrapers/grill_waits/scrape_grill_waits.py
@@ -2,6 +2,8 @@ import requests
 import json
 import datetime
 from firebase_admin import credentials, initialize_app, firestore
+import sys
+sys.append(".")  # your wish is my command
 
 ORDER_URL = "https://mange.dining.harvard.edu/php/getData.php"
 FORM_PAYLOAD = "formData%5BgetID%5D=waitTimes&formData%5Bhouses%5D%5B%5D=adams&formData%5Bhouses%5D%5B%5D=annenberg&formData%5Bhouses%5D%5B%5D=cabot&formData%5Bhouses%5D%5B%5D=currier&formData%5Bhouses%5D%5B%5D=dunster&formData%5Bhouses%5D%5B%5D=eliot&formData%5Bhouses%5D%5B%5D=kirkland&formData%5Bhouses%5D%5B%5D=leverett&formData%5Bhouses%5D%5B%5D=lowell&formData%5Bhouses%5D%5B%5D=mather&formData%5Bhouses%5D%5B%5D=pfoho&formData%5Bhouses%5D%5B%5D=quincy&formData%5Bhouses%5D%5B%5D=winthrop&userData%5BuserID%5D=11530&userData%5BisGuest%5D=0&userData%5BuserToken%5D=%242y%2410%24H2ebIAKXyaa3qfQTVfadUeAW%2FAiDtmanjcRfeVB1.qK9v4g8sjEne&userData%5BuserType%5D=user&userData%5BtimeStamp%5D=1569358524&otherData%5Blocation%5D=&otherData%5BuserAgent%5D=Mozilla%2F5.0+(X11%3B+Linux+x86_64)+AppleWebKit%2F537.36+(KHTML%2C+like+Gecko)+Chrome%2F77.0.3865.90+Safari%2F537.36&otherData%5Bvendor%5D=Google+Inc.&otherData%5Blanguage%5D=en-US"
