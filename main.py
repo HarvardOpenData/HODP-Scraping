0dfa71502ae1d8d625eedbfb99b8bb058dff9f81
Kevin Bi <keveloper@gmail.com>
2019-10-19 21:23:40 -0400

    add data and context argument because GCP is silly

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from scrapers.faculty_directory import scrape_faculty
 from scrapers.gocrimson import scrape_gocrimson
 from scrapers.grill_waits import scrape_grill_waits
 
-def crime_scraper():
+def crime_scraper(data, context):
     scrape_crime.main()
 
-def gocrimson_scraper():
+def gocrimson_scraper(data, context):
     scrape_gocrimson.main()
 
-def grill_waits_scraper():
+def grill_waits_scraper(data, context):
     scrape_grill_waits.main()
 
